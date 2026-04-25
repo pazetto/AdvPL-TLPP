d5cfc8ab2eec4d41917588702f35a35a55e9966e
Felipe Pazetto <felipe@pazetto.net>
2026-04-25 03:09:07 -0300

    Delete importador directory

--- importador/frontend/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Em producao o api_baseUrl vem de src/assets/data/appconfig.json (lido pela
- * protheus-lib-core). Para desenvolvimento standalone (ng serve), mantemos
- * este environment apenas para o flag `production`.
- */
-export const environment = {
-  production: false
-};
